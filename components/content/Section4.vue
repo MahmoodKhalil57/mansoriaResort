711df9b64e93cd322a0e022e58938c1974f10e49
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2025-01-05 19:32:08 +0000

    add section 4

---
+++ components/content/Section4.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center w-full bg-[#e6d7be]">
+    <div>
+      Hotel Villas
+    </div>
+    <div>
+      Six stand-alone luxury villas nestled in the hillside each with its own unique interior design.
+    </div>
+    <div class="w-[744px] h-[480px] overflow-hidden">
+      <NuxtImg width="1000" src="/sectionOneContentImage.jpeg" class="w-full object-cover" />
+    </div>
+    <div>
+      <p>
+        Take a Vacation from your Vacation
+      </p>
+      <NuxtLink>
+        Book a Villa
+      </NuxtLink>
+    </div>
+  </div>
+</template>
